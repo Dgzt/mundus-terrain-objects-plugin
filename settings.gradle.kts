448b7d8d17adfd4a04e1f6e6f16d486a33502afa
Zsuró Tibor <zsurotibor@gmail.com>
2025-05-31 18:33:10 +0200

    Set root project name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,5 +10,5 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
 }
 
-rootProject.name = "your-plugin"
+rootProject.name = "mundus-terrain-objects-plugin"
 include("plugin")
