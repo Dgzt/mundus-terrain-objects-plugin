04222bc594678ec78b12aa0472fd011f21db2023
Zsuró Tibor <zsurotibor@gmail.com>
2025-06-05 17:36:18 +0200

    Add texture grid widget

--- plugin/src/main/kotlin/com/github/dgzt/mundus/plugin/terrainobjects/plugin/creator/ComponentWidgetCreator.kt
+++ plugin/src/main/kotlin/com/github/dgzt/mundus/plugin/terrainobjects/plugin/creator/ComponentWidgetCreator.kt
@@ -2,10 +2,17 @@ package com.github.dgzt.mundus.plugin.terrainobjects.plugin.creator
 
 import com.github.dgzt.mundus.plugin.terrainobjects.runtime.component.TerrainObjectsComponent
 import com.mbrlabs.mundus.pluginapi.ui.RootWidget
+import com.mbrlabs.mundus.pluginapi.ui.WidgetAlign
 
 object ComponentWidgetCreator {
 
     fun setup(component: TerrainObjectsComponent, rootWidget: RootWidget) {
-        // TODD
+        rootWidget.addLabel("Objects:").setAlign(WidgetAlign.LEFT)
+        rootWidget.addRow()
+        rootWidget.addTextureGrid()
+        rootWidget.addRow()
+        rootWidget.addTextButton("Add Object") {
+            // TODO
+        }.setAlign(WidgetAlign.RIGHT)
     }
 }
