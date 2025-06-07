e5d5860edef9281e7dc127117742c01f7b08a468
Zsuró Tibor <zsurotibor@gmail.com>
2025-06-07 14:42:15 +0200

    Use listener at texture grid

--- plugin/src/main/kotlin/com/github/dgzt/mundus/plugin/terrainobjects/plugin/creator/ComponentWidgetCreator.kt
+++ plugin/src/main/kotlin/com/github/dgzt/mundus/plugin/terrainobjects/plugin/creator/ComponentWidgetCreator.kt
@@ -1,7 +1,11 @@
 package com.github.dgzt.mundus.plugin.terrainobjects.plugin.creator
 
+import com.badlogic.gdx.Gdx
 import com.github.dgzt.mundus.plugin.terrainobjects.runtime.component.TerrainObjectsComponent
+import com.github.dgzt.mundus.plugin.terrainobjects.runtime.constant.PluginConstants
 import com.mbrlabs.mundus.pluginapi.ui.RootWidget
+import com.mbrlabs.mundus.pluginapi.ui.TextureGrid
+import com.mbrlabs.mundus.pluginapi.ui.TextureGridListener
 import com.mbrlabs.mundus.pluginapi.ui.WidgetAlign
 
 object ComponentWidgetCreator {
@@ -9,7 +13,19 @@ object ComponentWidgetCreator {
     fun setup(component: TerrainObjectsComponent, rootWidget: RootWidget) {
         rootWidget.addLabel("Objects:").setAlign(WidgetAlign.LEFT)
         rootWidget.addRow()
-        val textureGrid = rootWidget.addTextureGrid().widget
+        var textureGrid: TextureGrid? = null
+
+        val textureGridListener = object : TextureGridListener {
+            override fun onChange(pos: Int) {
+                Gdx.app.log(PluginConstants.LOG_TAG, "Change: $pos")
+            }
+
+            override fun onRemove(pos: Int) {
+                Gdx.app.log(PluginConstants.LOG_TAG, "Remove: $pos")
+            }
+        }
+
+        textureGrid = rootWidget.addTextureGrid(true, true, textureGridListener).widget
         rootWidget.addRow()
         rootWidget.addTextButton("Add Object") {
             rootWidget.showModelAssetSelectionDialog {
--- runtime/src/main/java/com/github/dgzt/mundus/plugin/terrainobjects/runtime/constant/PluginConstants.java
+++ runtime/src/main/java/com/github/dgzt/mundus/plugin/terrainobjects/runtime/constant/PluginConstants.java
@@ -3,5 +3,6 @@ package com.github.dgzt.mundus.plugin.terrainobjects.runtime.constant;
 import com.mbrlabs.mundus.commons.scene3d.components.Component;
 
 public class PluginConstants {
+    public static final String LOG_TAG = "TerrainObjectsPlugin";
     public static final Component.Type TYPE = Component.Type.PARTICLE_SYSTEM; // TODO change it
 }
