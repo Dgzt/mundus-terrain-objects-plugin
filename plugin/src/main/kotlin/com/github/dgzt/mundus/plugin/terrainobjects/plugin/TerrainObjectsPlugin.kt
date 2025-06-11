4eac26d2f986823c93e974d270b31b08305969e6
Zsuró Tibor <zsurotibor@gmail.com>
2025-06-11 17:31:31 +0200

    Use terrain holder extension

--- plugin/src/main/kotlin/com/github/dgzt/mundus/plugin/terrainobjects/plugin/TerrainObjectsPlugin.kt
+++ plugin/src/main/kotlin/com/github/dgzt/mundus/plugin/terrainobjects/plugin/TerrainObjectsPlugin.kt
@@ -9,10 +9,10 @@ import com.github.dgzt.mundus.plugin.terrainobjects.runtime.converter.TerrainObj
 import com.mbrlabs.mundus.commons.mapper.CustomComponentConverter
 import com.mbrlabs.mundus.commons.scene3d.GameObject
 import com.mbrlabs.mundus.commons.scene3d.components.Component
-import com.mbrlabs.mundus.pluginapi.AssetExtension
 import com.mbrlabs.mundus.pluginapi.ComponentExtension
+import com.mbrlabs.mundus.pluginapi.ManagerHolderExtension
 import com.mbrlabs.mundus.pluginapi.MenuExtension
-import com.mbrlabs.mundus.pluginapi.manager.AssetManager
+import com.mbrlabs.mundus.pluginapi.manager.ManagerHolder
 import com.mbrlabs.mundus.pluginapi.ui.RootWidget
 import org.pf4j.Extension
 import org.pf4j.Plugin
@@ -55,9 +55,11 @@ class TerrainObjectsPlugin : Plugin() {
     }
 
     @Extension
-    class RecastAssetExtension : AssetExtension {
-        override fun assetManager(assetManager: AssetManager) {
-            PropertyManager.assetManager = assetManager
+    class TerrainObjectsManagerHolderExtension : ManagerHolderExtension {
+        override fun managerHolder(managerHolder: ManagerHolder) {
+            PropertyManager.assetManager = managerHolder.assetManager
         }
+
     }
+
 }
