ded980ba92fe712afde7aa1c5245e25da04d133b
Zsuró Tibor <zsurotibor@gmail.com>
2025-06-13 15:38:08 +0200

    Set rotation and scaling on selected model

--- plugin/src/main/kotlin/com/github/dgzt/mundus/plugin/terrainobjects/plugin/creator/ComponentWidgetCreator.kt
+++ plugin/src/main/kotlin/com/github/dgzt/mundus/plugin/terrainobjects/plugin/creator/ComponentWidgetCreator.kt
@@ -3,6 +3,7 @@ package com.github.dgzt.mundus.plugin.terrainobjects.plugin.creator
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.files.FileHandle
 import com.badlogic.gdx.graphics.g3d.ModelInstance
+import com.badlogic.gdx.math.Quaternion
 import com.badlogic.gdx.math.Vector3
 import com.github.dgzt.mundus.plugin.terrainobjects.plugin.PropertyManager
 import com.github.dgzt.mundus.plugin.terrainobjects.plugin.model.SelectedModel
@@ -68,9 +69,78 @@ object ComponentWidgetCreator {
     }
 
     private fun setupObjectsButtonPanel(gameObject: GameObject, objectButtonPanel: RootWidget, selectedModel: SelectedModel) {
-        objectButtonPanel.addTextButton("Add") {
+        val buttonsPanel = objectButtonPanel.addEmptyWidget().rootWidget
+        objectButtonPanel.addRow()
+        val optionsPanel = objectButtonPanel.addEmptyWidget().rootWidget
+
+        buttonsPanel.addTextButton("Add") {
+            optionsPanel.clearWidgets()
+            setupAddOptionPanel(selectedModel, optionsPanel)
+
             PropertyManager.toolManager.activateCustomTool(ToolListenerImpl(gameObject, selectedModel))
+        }.setPad(0f, 20f, 0f, 0f)
+
+        buttonsPanel.addTextButton("Remove") {
+            optionsPanel.clearWidgets()
+            // TODO
         }
+    }
+
+    private fun setupAddOptionPanel(selectedModel: SelectedModel, rootWidget: RootWidget) {
+        // rotation
+        rootWidget.addLabel("Rotation")
+        rootWidget.addRow()
+        rootWidget.addLabel("X").setPad(0f, 5f, 0f, 5f)
+        rootWidget.addSliderWithSpinnerWidget(0f, 359.9f, 0.1f, 1) {
+            selectedModel.rotationX = it
+            applyTransformations(selectedModel)
+        }
+        rootWidget.addRow()
+        rootWidget.addLabel("Y").setPad(0f, 5f, 0f, 5f)
+        rootWidget.addSliderWithSpinnerWidget(0f, 359.9f, 0.1f, 1) {
+            selectedModel.rotationY = it
+            applyTransformations(selectedModel)
+        }
+        rootWidget.addRow()
+        rootWidget.addLabel("Z").setPad(0f, 5f, 0f, 5f)
+        rootWidget.addSliderWithSpinnerWidget(0f, 359.9f, 0.1f, 1) {
+            selectedModel.rotationZ = it
+            applyTransformations(selectedModel)
+        }
+        rootWidget.addRow()
+
+        // scale
+        rootWidget.addLabel("Scale")
+        rootWidget.addRow()
+        rootWidget.addSpinner("X", 0.1f, Float.MAX_VALUE, 1.0f, 0.1f) {
+            selectedModel.scaleX = it
+            applyTransformations(selectedModel)
+        }.setPad(0f, 5f, 0f, 0f)
+        rootWidget.addRow()
+        rootWidget.addSpinner("Y", 0.1f, Float.MAX_VALUE, 1.0f, 0.1f) {
+            selectedModel.scaleY = it
+            applyTransformations(selectedModel)
+        }.setPad(0f, 5f, 0f, 0f)
+        rootWidget.addRow()
+        rootWidget.addSpinner("Z", 0.1f, Float.MAX_VALUE, 1.0f, 0.1f) {
+            selectedModel.scaleZ = it
+            applyTransformations(selectedModel)
+        }.setPad(0f, 5f, 0f, 0f)
+
+    }
+
+    private fun applyTransformations(selectedModel: SelectedModel) {
+        // Position
+        PropertyManager.selectedModelInstance?.transform?.idt()
+        PropertyManager.selectedModelInstance?.transform?.translate(selectedModel.posX, selectedModel.posY, selectedModel.posZ)
+
+        // Rotation
+        val quaternion = Quaternion()
+        quaternion.setEulerAngles(selectedModel.rotationY, selectedModel.rotationX, selectedModel.rotationZ)
+        PropertyManager.selectedModelInstance?.transform?.rotate(quaternion)
+
+        // Scaling
+        PropertyManager.selectedModelInstance?.transform?.scale(selectedModel.scaleX, selectedModel.scaleY, selectedModel.scaleZ)
     }
 
     private class TextureGridListenerImpl(
@@ -97,7 +167,7 @@ object ComponentWidgetCreator {
         }
     }
 
-    private class ToolListenerImpl(gameObject: GameObject, selectedModel: SelectedModel) : ToolListener {
+    private class ToolListenerImpl(gameObject: GameObject, val selectedModel: SelectedModel) : ToolListener {
 
         private val terrainComponent = gameObject.findComponentByType<TerrainComponent>(Component.Type.TERRAIN)
         private var tmpVector3 = Vector3()
@@ -114,7 +184,11 @@ object ComponentWidgetCreator {
             val ray = PropertyManager.viewportManager.getPickRay(screenX.toFloat(), screenY.toFloat())
             tmpVector3 = terrainComponent.getRayIntersection(tmpVector3, ray)
 
-            PropertyManager.selectedModelInstance?.transform?.setToTranslation(tmpVector3)
+            selectedModel.posX = tmpVector3.x
+            selectedModel.posY = tmpVector3.y
+            selectedModel.posZ = tmpVector3.z
+
+            applyTransformations(selectedModel)
         }
 
         override fun onDisabled() {
--- plugin/src/main/kotlin/com/github/dgzt/mundus/plugin/terrainobjects/plugin/model/SelectedModel.kt
+++ plugin/src/main/kotlin/com/github/dgzt/mundus/plugin/terrainobjects/plugin/model/SelectedModel.kt
@@ -5,4 +5,16 @@ import com.mbrlabs.mundus.commons.assets.ModelAsset
 class SelectedModel {
     var pos = -1
     lateinit var modelAsset: ModelAsset
+
+    var posX = 0f
+    var posY = 0f
+    var posZ = 0f
+
+    var rotationX = 0f
+    var rotationY = 0f
+    var rotationZ = 0f
+
+    var scaleX = 1f
+    var scaleY = 1f
+    var scaleZ = 1f
 }
